ca6ae42e5069df7713d60dae71fad87ab1a54ce1
Anup <anuppoudel60@gmail.comm>
2021-08-23 12:57:27 +1000

    table added

--- src/components/Covid.js
+++ src/components/Covid.js
@@ -1,9 +1,14 @@
 import React, { useEffect } from "react";
+import { Table } from "react-bootstrap";
 
 const Covid = () => {
   const getCovidData = async () => {
     try {
-      const res = await fetch("https://api.covid19india.org/data.json");
+      const res = await fetch(
+        "https://data.nsw.gov.au/data/dataset/0a52e6c1-bc0b-48af-8b45-d791a6d8e289/resource/f3a28eed-8c2a-437b-8ac1-2dab3cf760f9/download/covid-case-locations-20210823-1200.json"
+      );
+      const actualData = await res.json();
+      console.log(actualData.data.monitor);
     } catch (error) {
       console.log(error);
     }
@@ -14,8 +19,29 @@ const Covid = () => {
   }, []);
   return (
     <>
-      <h1> 🔴 LIVE</h1>
-      <h2>Covid 19 Dashboard for NSW</h2>
+      <div className="container-fluid mt-5">
+        <h1> 🔴 LIVE </h1>
+        <h2 className="mb-5">
+          <span className="font-weight-bold">NSW </span>COVID-19 venues
+        </h2>
+      </div>
+      <div className="table-responsive">
+        <table className="table table-hover">
+          <thead className="thead-dark">
+            <tr>
+              <th>Venue</th>
+              <th>Address</th>
+              <th>Suburb</th>
+              <th>Date</th>
+              <th>Time</th>
+              <th>Alert</th>
+              <th>Lon</th>
+              <th>Lat</th>
+              <th>Health Advice</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
     </>
   );
 };
