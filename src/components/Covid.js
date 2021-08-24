bddcece06b307ab3655b021a87caf4a2ae93ae25
Anup <anuppoudel60@gmail.comm>
2021-08-24 14:59:09 +1000

    api call done

--- src/components/Covid.js
+++ src/components/Covid.js
@@ -1,14 +1,17 @@
-import React, { useEffect } from "react";
-import { Table } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import "./Covid.style.css";
 
 const Covid = () => {
+  const [data, setData] = useState([]);
   const getCovidData = async () => {
     try {
       const res = await fetch(
         "https://data.nsw.gov.au/data/dataset/0a52e6c1-bc0b-48af-8b45-d791a6d8e289/resource/f3a28eed-8c2a-437b-8ac1-2dab3cf760f9/download/covid-case-locations-20210823-1200.json"
       );
       const actualData = await res.json();
-      console.log(actualData.data.monitor);
+      // console.log(actualData.data.monitor[0]);
+      setData(actualData.data.monitor);
     } catch (error) {
       console.log(error);
     }
@@ -19,29 +22,20 @@ const Covid = () => {
   }, []);
   return (
     <>
-      <div className="container-fluid mt-5">
-        <h1> 🔴 LIVE </h1>
-        <h2 className="mb-5">
-          <span className="font-weight-bold">NSW </span>COVID-19 venues
-        </h2>
-      </div>
-      <div className="table-responsive">
-        <table className="table table-hover">
-          <thead className="thead-dark">
-            <tr>
-              <th>Venue</th>
-              <th>Address</th>
-              <th>Suburb</th>
-              <th>Date</th>
-              <th>Time</th>
-              <th>Alert</th>
-              <th>Lon</th>
-              <th>Lat</th>
-              <th>Health Advice</th>
-            </tr>
-          </thead>
-        </table>
-      </div>
+      <h1> 🔴 LIVE </h1>
+      <h2>Covid 19 Venues across NSW </h2>
+      {data.map((currEle) => {
+        return (
+          <div className="container">
+            {" "}
+            Venue : {currEle.Venue} Address: {currEle.Address} Suburb :{" "}
+            {currEle.Suburb} Date : {currEle.Date} Time : {currEle.Time} Alert :{" "}
+            {currEle.Alert} Lon : {currEle.Lon} Lat : {currEle.Lat} HealthAdvice
+            : {currEle.HealthAdvice} transmissionvenues :{" "}
+            {currEle.transmissionvenues}
+          </div>
+        );
+      })}
     </>
   );
 };
